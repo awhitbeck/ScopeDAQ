2b313b5dc6284862075cda0ff6a1ef6275a332c6
awhitbeck <whitbeck.andrew@gmail.com>
2021-04-21 09:04:37 -0500

    removing analysis of channel2

--- analyze_source.py
+++ analyze_source.py
@@ -4,10 +4,11 @@ channel_list=[4]
 
 if __name__ == "__main__":
     run_name=sys.argv[1]
+    #run_name='path_to_my_data'
     dir_name=run_name.split('/')[-2]
     print dir_name
     df = load_data(run_name)
-
+    print df.head()
     #########################
     ## average pulse shape
     #########################
@@ -39,35 +40,37 @@ if __name__ == "__main__":
     for event in events : 
         event_filter = df['event_number']==event
         ch4_filter=df['channel']==4
-        ch1_filter=df['channel']==1
-        if len(df[event_filter&ch1_filter]['charge'].values) != 1 or len(df[event_filter&ch4_filter]['charge'].values) != 1 : 
-            print 'there are duplicates or not enough pulses in event',event
-        else : 
-            charge_pairs.append([df[event_filter&ch1_filter]['charge'].values[0],df[event_filter&ch4_filter]['charge'].values[0]])
+        #ch1_filter=df['channel']==1
+        #if len(df[event_filter&ch1_filter]['charge'].values) != 1 or len(df[event_filter&ch4_filter]['charge'].values) != 1 : 
+        #    print 'there are duplicates or not enough pulses in event',event
+        #else : 
+        #    charge_pairs.append([df[event_filter&ch1_filter]['charge'].values[0],df[event_filter&ch4_filter]['charge'].values[0]])
 
-    charge1=np.transpose(charge_pairs)[0]
-    charge1=np.divide(charge1,12.)
-    charge4=np.transpose(charge_pairs)[1]
-    charge4=np.divide(charge4,12.)
+    #charge1=np.transpose(charge_pairs)[0]
+    #charge1=np.divide(charge1,12.)
+    #charge4=np.transpose(charge_pairs)[1]
+    #charge4=np.divide(charge4,12.)
 
-    plt.hist2d(charge1,charge4,bins=[np.arange(0,400,10),np.arange(0,400,10)])
+    #plt.hist2d(charge1,charge4,bins=[np.arange(0,400,10),np.arange(0,400,10)])
     #plt.scatter(charge1,charge4,alpha=0.1)
-    plt.subplots_adjust(left=.14,bottom=.14,top=1.00)
-    plt.xlabel('Channel 1 Charge [PEs]')
-    plt.ylabel('Channel 4 Charge [PEs]')
-    plt.savefig(dir_name+'_charge_correlation.png')
+    #plt.subplots_adjust(left=.14,bottom=.14,top=1.00)
+    #plt.xlabel('Channel 1 Charge [PEs]')
+    #plt.ylabel('Channel 4 Charge [PEs]')
+    #plt.savefig(dir_name+'_charge_correlation.png')
     #plt.show()
-    plt.clf()
-    plt.cla()
+    #plt.clf()
+    #plt.cla()
 
+    print df.head()
+    
     #########################
     ### plot integrated charges
     #########################
     low_bin=0
-    high_bin=400
+    high_bin=1000
     bin_width=20
-    ch1_bins=plt.hist(charge1,bins=np.arange(low_bin,high_bin,bin_width),histtype='step')
-    ch4_bins=plt.hist(charge4,bins=np.arange(low_bin,high_bin,bin_width),histtype='step')
+    #ch1_bins=plt.hist(charge1,bins=np.arange(low_bin,high_bin,bin_width),histtype='step')
+    ch4_bins=plt.hist(df['charge'],bins=np.arange(low_bin,high_bin,bin_width),histtype='step')
     plt.legend(['Channel 1','Channel 4'])
     plt.xlabel('Charge [PEs]')
     plt.ylabel('Events')
@@ -77,19 +80,22 @@ if __name__ == "__main__":
     plt.clf()
     plt.cla()
 
-    print 'channel 1 mean',np.mean(charge1)
-    print 'channel 1 MPV',ch1_bins[1][np.argmax(ch1_bins[0])]+bin_width/2.
-    print 'channel 4 mean',np.mean(charge4)
+    #print 'channel 1 mean',np.mean(charge1)
+    #print 'channel 1 MPV',ch1_bins[1][np.argmax(ch1_bins[0])]+bin_width/2.
+    print 'channel 4 mean',np.mean(df['charge'])
     print 'channel 4 MPV',ch4_bins[1][np.argmax(ch4_bins[0])]+bin_width/2.
     
 
     #########################
     ### plot a pulse
     #########################
-    #t,v=get_pulse(20200627162332256,4)
-    #gr=r.TGraph(len(t),t,v)
-    #gr.Draw("AC")
+    t,v=get_pulse(20210415152133508,4)
 
+    can = r.TCanvas('can','can',500,500)
+    gr=r.TGraph(len(t),t,v)
+    gr.Draw("AC")
+    can.SaveAs("pulse.png")
+    
     # ch4_filter=df['channel']==4
     # ch1_filter=df['channel']==1
     # plt.hist(df[ch4_filter]['charge'].values,np.arange(0,5000,100),histtype='step')
